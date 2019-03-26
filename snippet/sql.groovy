c151f02ec2d1bb62ad0e2feceb6543b14dc8d0e5
fbabinthalia <francois.babin@thaliasolution.com>
2019-03-25 21:07:36 -0400

    Create sql.groovy

---
+++ snippet/sql.groovy
@@ -0,0 +1,19 @@
+import groovy.sql.Sql 
+
+class Example { 
+   static void main(String[] args) {
+      // Creating a connection to the database
+      def sql = Sql.newInstance('jdbc:mysql://localhost:3306/TESTDB', 'testuser',  
+         'test123', 'com.mysql.jdbc.Driver')
+			
+      def sqlstr = """CREATE TABLE EMPLOYEE ( 
+         FIRST_NAME CHAR(20) NOT NULL,
+         LAST_NAME CHAR(20),
+         AGE INT,
+         SEX CHAR(1),
+         INCOME FLOAT )""" 
+							
+      sql.execute(sqlstr);
+      sql.close() 
+   } 
+}
